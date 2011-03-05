686d76ec65c639486eea4d0ea20140fc97b089ec
Christian Feuersaenger <ludewich@users.sourceforge.net>
2011-03-05 13:53:24 +0100

    update unit tests

--- source/latex/pgfplots/pgfplotstest/unittests/pdfdiff.sh
+++ source/latex/pgfplots/pgfplotstest/unittests/pdfdiff.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+# USAGE: 
+# pdfdiff.sh <pdffile> <output-diff-file>
+#
+# It then compares <pdffile> with references/<pdffile> and writes the result to <output-diff-file>.
+# If the <output-diff-file> contains only "0 (0)", the images are equal on pixel-level.
+# If they are not equal, the script also generates <output-diff-file>.png containing the references.
+#
+# If supports multi-page pdf files.
 
 PDFFILE="$1"
 METRIC_FILE="$2"
 DIFF_IMAGE="$METRIC_FILE.png"
+
+VERBOSE=0
+
+function log()
+{
+	if [ $VERBOSE -eq 1 ]; then
+		echo $*
+	fi
+}
 
 if [ ! -f references/$PDFFILE ]; then 
 	echo "There is no reference solution for $PDFFILE. ";
@@ -14,18 +31,21 @@ fi
 PAGES=$((`pdfinfo $PDFFILE | grep 'Pages:' | cut -c 7-`));
 
 if [ $PAGES -eq 1 ]; then 
-	echo "Comparing $PDFFILE in 1-page-mode ... "; 
-	echo "compare -metric MAE $PDFFILE references/$PDFFILE $DIFF_IMAGE 2>$METRIC_FILE"; 
+	log "Comparing $PDFFILE in 1-page-mode ... "; 
+	log "compare -metric MAE $PDFFILE references/$PDFFILE $DIFF_IMAGE 2>$METRIC_FILE"; 
 	compare -metric MAE $PDFFILE references/$PDFFILE $DIFF_IMAGE 2>$METRIC_FILE; 
 else 
-	echo "Comparing $PDFFILE in multi-page-mode APPEND mode for the $PAGES pages ... "; 
-	echo "convert $PDFFILE -append $PDFFILE.png"; 
+	# this is more complicated because the 'compare' tools does not really support multi-page documents 
+	# (it collapses all pages to one and the comparison fails).
+	# So: make one HUGE image by appending all pages:
+	log "Comparing $PDFFILE in multi-page-mode APPEND mode for the $PAGES pages ... "; 
+	log "convert $PDFFILE -append $PDFFILE.png"; 
 	convert $PDFFILE -append $PDFFILE.png || exit 1; 
 	if [ "references/$PDFFILE" -nt "references/$PDFFILE.png" ]; then 
-		echo "convert references/$PDFFILE -append references/$PDFFILE.png"; 
+		log "convert references/$PDFFILE -append references/$PDFFILE.png"; 
 		convert references/$PDFFILE -append references/$PDFFILE.png || exit 1; 
 	fi; 
-	echo "compare -metric MAE $PDFFILE.png references/$PDFFILE.png $DIFF_IMAGE 2>$METRIC_FILE"; 
+	log "compare -metric MAE $PDFFILE.png references/$PDFFILE.png $DIFF_IMAGE 2>$METRIC_FILE"; 
 	compare -metric MAE $PDFFILE.png references/$PDFFILE.png $DIFF_IMAGE 2>$METRIC_FILE; 
 fi; 
 if [ $? -ne 0 ]; then 
--- source/latex/pgfplots/pgfplotstest/unittests/regressions.sh
+++ source/latex/pgfplots/pgfplotstest/unittests/regressions.sh
@@ -3,7 +3,9 @@
 
 PROBLEMS="`find . -maxdepth 1 -name "*.diff.png"`"
 for A in $PROBLEMS; do
-	echo "$A ..."
-	display $A
+	ACTUAL=${A%%.diff.png}.pdf
+	EXPECTED=references/$ACTUAL
+	echo "cycling through diff($A) $ACTUAL $EXPECTED ..."
+	display -delay 1 $A $ACTUAL $EXPECTED
 done
 	
