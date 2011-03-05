f5d534b45ff00662b29040e00b9b1c76103f1ede
Christian Feuersaenger <ludewich@users.sourceforge.net>
2011-03-05 20:59:17 +0100

    added further backw. switch

--- source/latex/pgfplots/pgfplotstest/unittests/regressions.sh
+++ source/latex/pgfplots/pgfplotstest/unittests/regressions.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 
 PROBLEMS="`find . -maxdepth 1 -name "*.diff.png"`"
+echo "Found regressions in the following files:"
+echo ${PROBLEMS//.diff.png/.tex}
+echo "I will now display each of them."
+	
 for A in $PROBLEMS; do
 	ACTUAL=${A%%.diff.png}.pdf
 	EXPECTED=references/$ACTUAL
 	echo "cycling through diff($A) $ACTUAL $EXPECTED ..."
-	display -delay 1 $A $ACTUAL $EXPECTED
+	display -delay 1 $A $ACTUAL $EXPECTED || break
 done
-	
+
