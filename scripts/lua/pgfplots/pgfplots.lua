f9ae677d710f0fb5df8885392eca10b6500c7730
Christian Feuersaenger <ludewich@users.sourceforge.net>
2012-04-06 15:21:53 +0200

    improved docs

--- scripts/lua/pgfplots/pgfplots.lua
+++ scripts/lua/pgfplots/pgfplots.lua
@@ -1,11 +1,17 @@
+-- Takes a table containing an arbitrary number of integers in the range 0..255 and converts it 
+-- into a binary stream of the corresponding binary chars.
+--
+-- @param charIndices a table containing 0...N arguments; each in the range 0..255
+--
+-- @return a string containing binary content, one byte for each input integer.
 function pgfplotsGetLuaBinaryStringFromCharIndices(charIndices)
 	-- unpack extracts only the indices (we can't provide a table to string.char).
 	-- note that pdf.immediateobj has been designed to avoid sanity checking for invalid UTF strings -
 	-- in other words: it accepts binary strings.
 	--
-	-- unfortunately, this here failsfor huge input tables:
-	-- pgfplotsretval=string.char(unpack(charIndices));
-	-- we have to chunk it:
+	-- unfortunately, this here fails for huge input tables:
+	--   pgfplotsretval=string.char(unpack(charIndices));
+	-- we have to create it incrementally using chunks:
 	local len = #charIndices;
 	local chunkSize = 7000;
 	local buf = {};
