45f24ac54b987ccb9434a407713347fc44bf4feb
Christian Feuersaenger <ludewich@users.sourceforge.net>
2012-11-01 12:10:09 +0100

    improved pgfplots manual date: it is now the revision + date

--- scripts/pgfplots/pgfplotsrevisionfile.sh
+++ scripts/pgfplots/pgfplotsrevisionfile.sh
@@ -5,6 +5,7 @@
 # The resulting macros define the current version of pgfplots.
 
 LATEST_TAG=`git describe --abbrev=0 --tags`
+REVISION=`git describe --tags HEAD`
 
 
 rm -f tex/generic/pgfplots/pgfplots.revision.tex
@@ -17,10 +18,7 @@ echo '\\catcode`\\+=12' >> tex/generic/pgfplots/pgfplots.revision.tex
 echo '\\catcode`\\-=12' >> tex/generic/pgfplots/pgfplots.revision.tex
 
 # this is the REVISION, i.e. the unique hash of the changeset.
-echo -n '\gdef\pgfplotsrevision{' >> tex/generic/pgfplots/pgfplots.revision.tex
-# the 'echo' strips the trailing newline.
-echo -n "`git describe --tags HEAD`" >> tex/generic/pgfplots/pgfplots.revision.tex
-echo '}' >>  tex/generic/pgfplots/pgfplots.revision.tex
+echo '\gdef\pgfplotsrevision{'"$REVISION}" >> tex/generic/pgfplots/pgfplots.revision.tex
 
 # this is the public version name. It corresponds to the latest tag name in the git repo.
 echo '\gdef\pgfplotsversion{'"$LATEST_TAG}" >> tex/generic/pgfplots/pgfplots.revision.tex
@@ -31,8 +29,13 @@ echo -n '\gdef\pgfplotsversiondatetime{' >> tex/generic/pgfplots/pgfplots.revisi
 git log -n 1 "$LATEST_TAG" --pretty=format:"%ci" >> tex/generic/pgfplots/pgfplots.revision.tex
 echo '}' >>  tex/generic/pgfplots/pgfplots.revision.tex
 
+echo -n '\gdef\pgfplotsrevisiondatetime{' >> tex/generic/pgfplots/pgfplots.revision.tex
+git log -n 1 "$REVISION" --pretty=format:"%ci" >> tex/generic/pgfplots/pgfplots.revision.tex
+echo '}' >>  tex/generic/pgfplots/pgfplots.revision.tex
+
 # convert to latex format YYYY/MM/DD :
 echo '\gdef\pgfplots@glob@TMPa#1-#2-#3 #4\\relax{#1/#2/#3}' >>  tex/generic/pgfplots/pgfplots.revision.tex
 echo '\xdef\pgfplotsversiondate{\expandafter\pgfplots@glob@TMPa\pgfplotsversiondatetime\\relax}' >>  tex/generic/pgfplots/pgfplots.revision.tex
+echo '\xdef\pgfplotsrevisiondate{\expandafter\pgfplots@glob@TMPa\pgfplotsrevisiondatetime\\relax}' >>  tex/generic/pgfplots/pgfplots.revision.tex
 echo '\endgroup' >> tex/generic/pgfplots/pgfplots.revision.tex
 exit 0
