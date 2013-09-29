84c89f4b9157211d8676a765a157f63d6c268756
Christian Feuersaenger <ludewich@users.sourceforge.net>
2013-09-29 18:13:53 +0200

    fixed test issue

--- source/latex/pgfplots/pgfplotstest/regressiontests/compile_external_make.sh
+++ source/latex/pgfplots/pgfplotstest/regressiontests/compile_external_make.sh
@@ -8,15 +8,17 @@ echo "   [cleanup...]"
 rm -f $@-figure*
 #ls $@-figure*
 
+
+echo "   [first compile...]"
+pdflatex -interaction batchmode -halt-on-error "$@" || ( rm -f $@.pdf; exit 1 )
+
+
 # This line here appears to be unmotivated. However, it has its uses during
 # the compilation of REFERENCEs: this step has search path .:.. . Consequently,
 # it will find the .md5 file of the actual - and will fail.
 # This here avoids the problem by means:
-touch $@-figure0.md5
-
-
-echo "   [first compile...]"
-pdflatex -interaction batchmode -halt-on-error "$@" || ( rm -f $@.pdf; exit 1 )
+echo "%.md5:" >> $@.makefile
+echo -e "\ttouch $$@" >> $@.makefile
 
 echo "   [makefile for images...]"
 make -f "$@.makefile" || ( rm -f $@.pdf; exit 1 )
